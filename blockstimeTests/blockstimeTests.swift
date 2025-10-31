cef28daa6e51449ba4e895439bd0fd137873bae7
Your Name <your_email@example.com>
2025-10-31 10:33:03 +0800

    Rename project from LegoTimePlanner to blockstime

---
+++ blockstimeTests/blockstimeTests.swift
@@ -0,0 +1,16 @@
+//
+//  blockstimeTests.swift
+//  blockstimeTests
+//
+//  Created by Alex Su on 2025/10/30.
+//
+
+import Testing
+
+struct blockstimeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ blockstime_bak/blockstimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  blockstimeApp.swift
+//  blockstime
+//
+//  Created by Alex Su on 2025/10/30.
+//
+
+import SwiftUI
+
+@main
+struct blockstimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
